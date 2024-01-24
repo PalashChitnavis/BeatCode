2f25655f8e4245271387cfcfed1da28465ff20b2
Palash Chitnavis <palashchitnavis@gmail.com>
2024-01-24 23:06:33 +0530

    frontend changes and backend creation

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/api/run", (req, res) => {
+        const { code, userInput, language } = req.body;
+        console.log("Received Data:", { code, userInput, language });
+        res.send({ code, userInput, language });
+});
+
+app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+});
--- frontend/src/components/NavBar/RunButton.jsx
+++ frontend/src/components/NavBar/RunButton.jsx
@@ -2,11 +2,22 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { useBody } from "../../context/BodyContext";
+import axios from "axios";
 import "./RunButton.css";
 const RunButton = () => {
         const { body } = useBody();
-        const handleClick = () => {
-                console.log(body);
+        const reqBody = {
+                code: body.code,
+                userInput: body.userInput,
+                language: body.language,
+        };
+        const handleClick = async () => {
+                try {
+                        const res = await axios.post("http://localhost:3000/api/run", reqBody);
+                        console.log("Backend Response:", res.data);
+                } catch (err) {
+                        console.error("Error:", err.message);
+                }
         };
         return (
                 <button className="css-button-arrow--green" onClick={handleClick}>
