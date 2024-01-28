670dfc44103d89305377b7e0626e89a0bc09887c
Mahesh Suryawanshi <140623030+maheshcodes12@users.noreply.github.com>
2024-01-28 20:02:57 +0530

    Created Home Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import OnlineCompiler from "./pages/OnlineCompiler/OnlineCompiler";
 import PracticeProblems from "./pages/PracticeProblems/PracticeProblems";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./main.css";
+import HomePage from "./pages/HomePage/HomePage";
 function App() {
         return (
                 <>
                         {" "}
                         <Router>
                                 <Routes>
-                                        <Route path="/" element={<h1>Home Page</h1>} />
+                                        <Route path="/" element={<HomePage/>} />
                                         <Route path="/onlinecompiler" element={<OnlineCompiler />} />
                                         <Route path="/practiceproblems" element={<PracticeProblems />} />
                                 </Routes>
