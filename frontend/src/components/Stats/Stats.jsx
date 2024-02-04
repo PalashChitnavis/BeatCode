864129abb727c06b0ba4dcdc2d5cdd2ab3850129
Mahesh Suryawanshi <140623030+maheshcodes12@users.noreply.github.com>
2024-02-04 22:26:26 +0530

    Small variable changes

--- frontend/src/components/Stats/Stats.jsx
+++ frontend/src/components/Stats/Stats.jsx
@@ -4,32 +4,34 @@ import ProgressBar from "../ProgressBar/ProgressBar";
 
 export default function Stats() {
         const TotalProblems = 12;
-        const solved = 3;
+        const easyProblemsSolved = 3;
+        const mediumProblemsSolved = 5;
+        const hardProblemsSolved = 7;
         return (
                 <div className="font-bold p-4  bg-black rounded-lg">
                         <div className="text-white p-4 pt-0 flex justify-center text-xl">Stats</div>
                         <div className="text-green-600 flex justify-between">
                                 Easy
                                 <div>
-                                        {solved}/{TotalProblems}
+                                        {easyProblemsSolved}/{TotalProblems}
                                 </div>
                         </div>
-                        <ProgressBar progress={solved / TotalProblems} color="green" />
+                        <ProgressBar progress={easyProblemsSolved/ TotalProblems} color="green" />
                         <div className="text-yellow-500 flex justify-between">
                                 Medium
                                 <div>
-                                        {solved}/{TotalProblems}
+                                        {mediumProblemsSolved}/{TotalProblems}
                                 </div>
                         </div>
-                        <ProgressBar progress={solved / TotalProblems} color="yellow" />
+                        <ProgressBar progress={mediumProblemsSolved / TotalProblems} color="yellow" />
 
                         <div className="text-red-600 flex justify-between">
                                 Hard
                                 <div>
-                                        {solved}/{TotalProblems}
+                                        {hardProblemsSolved}/{TotalProblems}
                                 </div>
                         </div>
-                        <ProgressBar progress={solved / TotalProblems} color="red" />
+                        <ProgressBar progress={hardProblemsSolved / TotalProblems} color="red" />
                 </div>
         );
 }
