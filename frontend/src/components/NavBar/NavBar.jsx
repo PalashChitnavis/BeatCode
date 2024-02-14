68120799048cdee328d07ae489cc28db71400a4d
Palash Chitnavis <palashchitnavis@gmail.com>
2024-02-14 17:33:01 +0530

    Images Added

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -7,7 +7,6 @@ import RunButton from "./RunButton";
 import ResetCode from "./ResetCode";
 import Settings from "./Settings";
 import { useBody } from "../../context/BodyContext";
-import ShareCode from "../ShareCode/ShareCode";
 import { useLocation } from "react-router-dom";
 import "./NavBar.css";
 const NavBar = ({ socket, roomID }) => {
@@ -18,28 +17,15 @@ const NavBar = ({ socket, roomID }) => {
                         <div className="w-[15%] h-[95%] flex items-center justify-center">
                                 <LanguageSelector socket={socket} roomID={roomID} />
                         </div>
-                        <div className="w-[60%] flex ">
-                                {location.pathname === "/onlinecompiler" ? (
-                                        <div className="w-[30%]">
-                                                <ShareCode />
-                                        </div>
-                                ) : (
-                                        <div className="w-[0%] h-[0%]"></div>
-                                )}
-                                <div
-                                        className={`flex items-center justify-end  w-[${
-                                                location.pathname != "/onlinecompiler" ? "100%" : "70%"
-                                        }]  bg-neutral-800`}
-                                >
-                                        <div className="w-[25%]">
-                                                <ResetCode />
-                                        </div>
-                                        <div className="w-[25%]">
-                                                <Settings />
-                                        </div>
-                                        <div className="h-[35px] w-[50%]">
-                                                <RunButton />
-                                        </div>
+                        <div className="w-[50%] flex items-center">
+                                <div className="w-[30%]">
+                                        <ResetCode />
+                                </div>
+                                <div className="w-[30%]">
+                                        <Settings />
+                                </div>
+                                <div className="h-[35px] w-[50%]">
+                                        <RunButton />
                                 </div>
                         </div>
                 </div>
